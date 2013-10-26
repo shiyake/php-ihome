e05102a5fa80afaecb207c573fa6a5c1127ad54a
Jianfei Wang <jfwang213@gmail.com>
2013-10-26 22:56:37 +0800

    refine search

--- ihome/source/space_search.php
+++ ihome/source/space_search.php
@@ -1,6 +1,6 @@
 <?php
 if (empty($_GET['type'])) {
-    $search_type = 'blog';
+    $search_type = 'user';
 } else {
     $search_type = $_GET['type'];
 }
@@ -9,42 +9,81 @@ if (isset($_GET['query'])) {
 } else {
     $query = '';
 }
-$search_url = 'http://jianfei.bestgames7.com/solr/select/?q=' . urlencode("search_text:".$query." AND type:" . $search_type) . "&wt=json";
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $search_url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-$data = curl_exec($curl);
-curl_close($curl);
-$data = json_decode($data, true);
-$items = $data['response']['docs'];
-$all_item_count = $data['response']['numFound'];
-$start = $data['response']['start'];
-if ($search_type == 'blog') {
-    $summarylen = 300;
-    foreach ($items as $key => $value) {
-        $itemids[] = $value['blogid'];
+
+$page = empty($_GET['page'])?1:intval($_GET['page']);
+if($page<1) $page=1;
+$perpage = 10;
+$start = ($page - 1) * $perpage;
+
+if (strlen($query) > 0) {
+    if ($search_type == 'blog' || $search_type == 'event' || $search_type == 'poll' || $search_type == 'doing') {
+        $order_url = "&sort=".urlencode("score desc, dateline desc");
+    } else {
+        $order_url = "";
     }
-    $idstr = join(',', $itemids);
-    $query = $_SGLOBAL['db']->query("SELECT bf.message, bf.target_ids, bf.magiccolor, b.* FROM ".tname('blog')." b
-        LEFT JOIN ".tname('blogfield')." bf ON bf.blogid=b.blogid
-        WHERE b.blogid in ($idstr)");
-    $list = array();
-    $pricount++;
-    while ($value = $_SGLOBAL['db']->fetch_array($query)) {
-        if(ckfriend($value['uid'], $value['friend'], $value['target_ids'])) {
-            realname_set($value['uid'], $value['username']);
-            if($value['friend'] == 4) {
-                $value['message'] = $value['pic'] = '';
-            } else {
-                $value['message'] = getstr($value['message'], $summarylen, 0, 0, 0, 0, -1);
-            }
-            if($value['pic']) $value['pic'] = pic_cover_get($value['pic'], $value['picflag']);
-            $list[] = $value;
-        } else {
-            $pricount++;
+    $search_url = 'http://jianfei.bestgames7.com/solr/select/?q=' . urlencode("search_text:".$query." AND type:" . $search_type) . "&wt=json&start=".$start."&rows=".$perpage.$order_url;
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $search_url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($curl);
+    curl_close($curl);
+
+    $data = json_decode($data, true);
+    $items = $data['response']['docs'];
+    $all_item_count = $data['response']['numFound'];
+    $theurl = "space.php?query=$query&do=search&type=$search_type";
+} else {
+    $items = array();
+    $all_item_count = 0;
+    $theurl = '';
+}
+
+$multi = multi($all_item_count, $perpage, $page, $theurl);
+
+function resort($ids, $items) {
+    $results = array();
+    foreach ($ids as $id) {
+        if (array_key_exists($id, $items)) {
+            $results[] = $items[$id];
         }
     }
+    return $results;
+}
 
+
+if ($search_type == 'blog') {
+    if (count($items) > 0) {
+        $summarylen = 300;
+        foreach ($items as $key => $value) {
+            $itemids[] = $value['blogid'];
+        }
+        $idstr = join(',', $itemids);
+        $query = $_SGLOBAL['db']->query("SELECT bf.message, bf.target_ids, bf.magiccolor, b.* FROM ".tname('blog')." b
+            LEFT JOIN ".tname('blogfield')." bf ON bf.blogid=b.blogid
+            WHERE b.blogid in ($idstr)");
+        $list = array();
+        $pricount++;
+        while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+            if(ckfriend($value['uid'], $value['friend'], $value['target_ids'])) {
+                realname_set($value['uid'], $value['username']);
+                if($value['friend'] == 4) {
+                    $value['message'] = $value['pic'] = '';
+                } else {
+                    $value['message'] = getstr($value['message'], $summarylen, 0, 0, 0, 0, -1);
+                }
+                if($value['pic']) $value['pic'] = pic_cover_get($value['pic'], $value['picflag']);
+                $list[$value['blogid']] = $value;
+            } else {
+                $pricount++;
+            }
+        }
+    } else {
+        $list = array();
+    }
+    realname_get();
+
+    $list = resort($itemids, $list);
 	$_TPL['css'] = 'blog';
     include_once template("space_search_blog");
 } else if ($search_type == 'user') {
@@ -71,7 +110,9 @@ if ($search_type == 'blog') {
             $list[$key] = $value;
         }
     }
+    realname_get();
 
+    $list = resort($itemids, $list);
     include_once template("space_search_user");
 } else if ($search_type == 'event') {
     foreach ($items as $key => $value) {
@@ -99,6 +140,8 @@ if ($search_type == 'blog') {
             $eventlist[$event['eventid']] = $event;
         }
     }
+    $eventlist = resort($itemids, $eventlist);
+    realname_get();
 	$_TPL['css'] = 'event';
     include_once template("space_search_event");
 } else if ($search_type == 'album') {
@@ -116,9 +159,11 @@ if ($search_type == 'blog') {
             } else {
                 $value['pic'] = 'image/nopublish.jpg';
             }
-            $list[] = $value;
+            $list[$value['albumid']] = $value;
         }
     }
+    $list = resort($itemids, $list);
+    realname_get();
 	$_TPL['css'] = 'album';
     include_once template("space_search_album");
 } else if ($search_type == 'poll') {
@@ -137,10 +182,11 @@ if ($search_type == 'blog') {
             }
             realname_set($value['uid'], $value['username']);
             $value['option'] = unserialize($value['option']);
-            $list[] = $value;
+            $list[$value['pid']] = $value;
         }
     }
-
+    $list = resort($itemids, $list);
+    realname_get();
 	$_TPL['css'] = 'poll';
     include_once template("space_search_poll");
 } else if ($search_type == 'mtag') {
@@ -156,10 +202,61 @@ if ($search_type == 'blog') {
             if(empty($value['pic'])) $value['pic'] = 'image/nologo.jpg';
             $tagids[] = $value['tagid'];
             $tagnames[$value['tagid']] = $value['tagname'];
-            $list[] = $value;
+            $list[$value['tagid']] = $value;
         }
     }
+
+    $list = resort($itemids, $list);
+    realname_get();
 	$_TPL['css'] = 'thread';
     include_once template("space_search_mtag");
+} else if ($search_type == 'doing') {
+    foreach ($items as $key => $value) {
+        $itemids[] = $value['doid'];
+    }
+    $dolist = array();
+    if (count($itemids) > 0) {
+        $idstr = join(',', $itemids);
+        $query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('doing')." 
+            WHERE doid in ($idstr)");
+        while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+            realname_set($value['uid'], $value['username']);
+            $doids[] = $value['doid'];
+            $dolist[$value['doid']] = $value;
+        }
+    }
+    //回复
+    if($doids) {
+        
+        include_once(S_ROOT.'./source/class_tree.php');
+        $tree = new tree();
+        
+        $values = array();
+        $query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('docomment')." USE INDEX(dateline) WHERE doid IN (".simplode($doids).") ORDER BY dateline");
+        while ($value = $_SGLOBAL['db']->fetch_array($query)) {
+            realname_set($value['uid'], $value['username']);
+            $newdoids[$value['doid']] = $value['doid'];
+            if(empty($value['upid'])) {
+                $value['upid'] = "do$value[doid]";
+            }
+            $tree->setNode($value['id'], $value['upid'], $value);
+        }
+    }
+    foreach ($newdoids as $cdoid) {
+        $values = $tree->getChilds("do$cdoid");
+        foreach ($values as $key => $id) {
+            $one = $tree->getValue($id);
+            $one['layer'] = $tree->getLayer($id) * 2 - 2;
+            $one['style'] = "padding-left:{$one['layer']}em;";
+            if($_GET['highlight'] && $one['id'] == $_GET['highlight']) {
+                $one['style'] .= 'color:red;font-weight:bold;';
+            }
+            $clist[$cdoid][] = $one;
+        }
+    }
+    $dolist = resort($itemids, $dolist);
+    $_TPL['css'] = 'doing';
+    realname_get();
+    include_once template("space_search_doing");
 }
 ?>
