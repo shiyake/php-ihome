7e15a9365875baf12cfad04c9c85464fa5ba51f5
songjinghe <songjignhe@163.com>
2014-05-26 17:14:23 +0800

    新增针对WSC应用的自动授权，授权权限是写死的。直接跳回应用的授权回调地址。 这样不用和应用大厅有交集，也就不用登录CAS。。。。

--- ihome/plugin/iauth/login.php
+++ ihome/plugin/iauth/login.php
@@ -17,7 +17,7 @@ catch(IAuthException $e){
     showError($e);
     }
 
-header('Location:'.$callBack);
+header('Location: '.$callBack);
 exit();
 
 
@@ -29,27 +29,42 @@ function SSOlogin( $appid, $state, $uid ){
     if (!empty($state)){
         Check( $state,'state' );
         Check( $uid, 'uid' );
+
         $authed = CheckUserAuthed( $appid, $uid );
-        if ($authed==false){
-            if ((!empty($_GET['s'])) && ($_GET['s']=='1')){
-                header('Location: '.IAUTH_SIMPLE_AUTH_CONFIRM_PAGE.'&appsid='.$appid.'&state='.$state);
-                exit();
-            }
-            header('Location:'.IAUTH_APP_INFO_PAGE.'&appsid='.$appid.'&state='.$state.'#confirm');
-            exit();
-            }
         $appType = GetAppInfo( $appid, 'app_type' );
+	$autoAuth = GetAppInfo( $appid, 'auto_auth' );
 
-        if ( $appType =='WSC' ){
-            $loginCallBack = GetAppInfo( $appid, 'login_url' );
-            $verifier = newVerifier('login', $appid, $uid, 'FROM_CLIENT','','','', $state );
-            return $loginCallBack .'?verifier='. $verifier.'&state='.$state;
-            }
-        }
-    header('Location:'.IAUTH_APP_INFO_PAGE.'&appsid='.$appid);
-    exit();
+	if ($appType=='WSC'){
+
+	    if (($authed==FALSE) && ($authAuth==FALSE)){ //用户没有授权，且应用不是自动授权，跳转到应用大厅
+		if ((!empty($_GET['s'])) && ($_GET['s']=='1')){ /* 加参数跳转到精简版界面 */
+		    return IAUTH_SIMPLE_AUTH_CONFIRM_PAGE.'&appsid='.$appid.'&state='.$state ;
+		    }
+		return IAUTH_APP_INFO_PAGE.'&appsid='.$appid.'&state='.$state.'#confirm';
+		}
+
+
+	    if (($authed==FALSE) && ($authAuth==TRUE)){ //用户没有授权，但是应用是自动授权，直接跳回应用的auth_call_back
+		$authCallBack = GetAppInfo( $appid, 'call_back' );
+		$rights = Check( '2:3:7:11', 'rights' ); /* 由于没有应用大厅，权限被写死 */
+		$faile_t='2036-12-31 23:59:59';
+		$verifier = newVerifier('auth', $appid, $uid, $rights, $faile_t,'','',$state);
+		accessLog('AUTH '.$appid.' '.$uid.' 2:3:7:11 '.$faile_t.' '.$state);
+		return $authCallBack .'?verifier='. $verifier.'&state='.$state;
+		}
+
+	    if ( $authed==TRUE ){ /* 用户已经授权，直接跳转回login_call_back */
+		$loginCallBack = GetAppInfo( $appid, 'login_url' );
+		$verifier = newVerifier('login', $appid, $uid, 'FROM_CLIENT','','','', $state );
+		return $loginCallBack .'?verifier='. $verifier.'&state='.$state;
+		}
+
+	    } /* END WSC */
+
+        }     /* END !EMPTY state */
+
+    /* 其他情况，跳到应用大厅 */
+    return IAUTH_APP_INFO_PAGE.'&appsid='.$appid;
     }
-
-
 
 ?>
