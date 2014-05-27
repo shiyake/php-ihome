8b85e95d95c4b7c2603db93250340e54a94e3dfa
ancon <woaizhongfeiyu@gmail.com>
2014-05-28 04:15:49 +0800

    1，微信发送数据； 2，tracking程序和页面的优化； 3，首页上墙的优化； 我说一句，架构师，你可以加入进来开发么？ 这加载速度很慢你知道么？

--- ihome/api/wxsendmsg.php
+++ ihome/api/wxsendmsg.php
@@ -38,18 +38,53 @@ else
 		}
 		else
 		{
+			$Query = $_SGLOBAL['db']->query("SELECT uid,wallname,`check` FROM ".tname('wall')." WHERE id = '$WallId' ");
+			if ($Value = $_SGLOBAL['db']->fetch_array($Query)) {
+				$apply = $Value['uid'];
+				$check = $Value['check'];
+				$WallTitle = $Value['wallname'];
+			}
+			$isfounder = ckfounder($uid);
+			if($check || $isfounder || $uid == 144 || $uid == $apply){
+				$pass = 1;
+			}			
             $setarr = array(
                 'uid' => $uid,
                 'pass' =>1,
-                'play' = 1,
                 'username' => $username,
                 'message' => $message,
                 'wallid' => $wallid,
                 'ip' => 'weixin',
                 'timeline' => $_SGLOBAL['timestamp'],
+                'fromdevice' => 'wechat'
             );
             //入库
             $newwallid = inserttable('wallfield', $setarr, 1);
+
+            if ($check > 0 && $pass > 0) {
+			$message = "<a href=\"plugin.php?pluginid=wall&wallid=".$WallId."&ac=track\">#".$WallTitle."#</a> ".$message;
+			$feedarr = array(
+					'appid' => UC_APPID,
+					'icon' => 'doing',
+					'uid' => $uid,
+					'username' => $_SGLOBAL['supe_username'],
+					'dateline' => $_SGLOBAL['timestamp'],
+					'title_template' => cplang('feed_doing_title'),
+					'title_data' => saddslashes(serialize(sstripslashes(array('message'=>$message)))),
+					'body_template' => '',
+					'body_data' => '',
+					'id' => $newwallid,
+					'idtype' => 'wallid'
+				);
+				$feedarr['hash_template'] = md5($feedarr['title_template']."\t".$feedarr['body_template']);
+				$feedarr['hash_data'] = md5($feedarr['title_template']."\t".$feedarr['title_data']."\t".$feedarr['body_template']."\t".$feedarr['body_data']);
+				$FeedId = inserttable('feed', $feedarr,1);
+				
+				if($FeedId)
+				{
+					updatetable('wallfield', array('feedid'=>$FeedId), array('id'=>$id));
+				}
+			}			
 			returnResponse(0, "ok");
 		}
 	}
