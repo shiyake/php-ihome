01b34a6aee54fa5bcde361765c28f8b0369a704f
wangbailin <wangbailin@limijiaoyin.com>
2014-07-04 19:27:56 +0800

    修复反馈done

--- ihome/admin/tpl/recommendpublic.js
+++ ihome/admin/tpl/recommendpublic.js
@@ -1,6 +1,21 @@
 (function(){
 	
 	jQuery(function(){
+		jQuery(document).keypress(function(e){
+			if(e.which==13)	{
+				jQuery(".query_button").click();
+			}
+		});
+		jQuery(".query_input").keyup(function(e){
+			if(jQuery(this).val()!="" && e.which!=13)	{
+				jQuery(".query_button").click();
+			}
+			else if(jQuery(this).val()==""){
+				jQuery(".query_div").css({
+					"display":"none"
+				});
+			}
+		});
 		jQuery(".query_button").click(function(){
 			jQuery(".query_div").css({"display":"block"});
 			jQuery.post("admincp.php?ac=recommendpublic&op=query",
