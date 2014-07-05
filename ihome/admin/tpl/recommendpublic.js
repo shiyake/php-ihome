55df391997414d7dd466cc5a4e17c5865ea1b158
wangbailin <wangbailin@limijiaoyin.com>
2014-07-05 13:35:18 +0800

    公共主页推荐v1

--- ihome/admin/tpl/recommendpublic.js
+++ ihome/admin/tpl/recommendpublic.js
@@ -1,16 +1,15 @@
 (function(){
 	
 	jQuery(function(){
-		jQuery(document).keypress(function(e){
+		jQuery(".query_input").keypress(function(e){
 			if(e.which==13)	{
 				jQuery(".query_button").click();
 			}
 		});
+		
+	
 		jQuery(".query_input").keyup(function(e){
-			if(jQuery(this).val()!="" && e.which!=13)	{
-				jQuery(".query_button").click();
-			}
-			else if(jQuery(this).val()==""){
+			if(jQuery(this).val()==""){
 				jQuery(".query_div").css({
 					"display":"none"
 				});
@@ -18,13 +17,16 @@
 		});
 		jQuery(".query_button").click(function(){
 			jQuery(".query_div").css({"display":"block"});
-			jQuery.post("admincp.php?ac=recommendpublic&op=query",
-				{
-					"query":jQuery(".query_input").val()
-				},
-				function(data){
-					jQuery(".public_message").html(jQuery(data).find(".public_message").html());
+			if(jQuery(".query_input").val())	{
+				jQuery.post("admincp.php?ac=recommendpublic&op=query",
+					{
+						"query":jQuery(".query_input").val()
+					},
+					function(data){
+						jQuery(".public_message").html(jQuery(data).find(".public_message").html());
 				});
+			}
 		});
+
 	});
 })();
