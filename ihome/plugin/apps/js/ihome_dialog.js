200e2b92230553e57854b013219af9ee40476f4f
PopPopRet <lance.through@the.heart>
2014-08-15 15:30:52 +0800

    refine detail

--- ihome/plugin/apps/js/ihome_dialog.js
+++ ihome/plugin/apps/js/ihome_dialog.js
@@ -1,31 +1,31 @@
 /*ihome_dialog.js*/
 
-jq(function() {
+jQuery(function() {
 	var html = "<div id=\"msg-container\"><div id=\"msg-box\"></div></div>";
-	jq("body").append(html);
+	jQuery("body").append(html);
 	
 });
 
 
 
 function resetDialog(){
-	var msg_box = jq("#msg-box");
-	var msg = jq("#msg");
+	var msg_box = jQuery("#msg-box");
+	var msg = jQuery("#msg");
 	
 	//取得浏览器窗口高宽
-	var fullheight = jq(window).height();
-	var fullwidth = jq(window).width();
+	var fullheight = jQuery(window).height();
+	var fullwidth = jQuery(window).width();
 	
 	//取得子DIV器高宽
-	var msgWidth = jq(msg).width();
-	var msgHeight = jq(msg).height();
+	var msgWidth = jQuery(msg).width();
+	var msgHeight = jQuery(msg).height();
 	
 	//计算top和left
 	var msgTop = (fullheight - msgHeight - 200) / 2;
 	var msgLeft = (fullwidth - msgWidth) / 2;
 	
 	msg_box.css("top" , msgTop);
-	if(!(jq.browser.msie&&(jq.browser.version == "7.0"))){
+	if(!(jQuery.browser.msie&&(jQuery.browser.version == "7.0"))){
 		msg_box.css("left" , msgLeft);
 	} 
 	msg_box.css("width" , msgWidth + "px");
@@ -36,22 +36,22 @@ function resetDialog(){
 
 function showDialog(){
 	resetDialog();
-	jq("#msg-container").fadeIn(500);
+	jQuery("#msg-container").fadeIn(500);
 	//可拖动
-	jq("#msg-box").draggable({opacity: 0.6,cursor: 'move',handle:'#handel'});
+	jQuery("#msg-box").draggable({opacity: 0.6,cursor: 'move',handle:'#handel'});
 	//监听esc按键
-	jq(document).bind("keydown", "esc", function (ev) { closeDialog();});
+	jQuery(document).bind("keydown", "esc", function (ev) { closeDialog();});
 }
 
 function closeDialog(){
-	jq("#msg-container").fadeOut(500 ,function(){
-		jq("#msg-box").empty();
+	jQuery("#msg-container").fadeOut(500 ,function(){
+		jQuery("#msg-box").empty();
 	});
 	
 }
 
 function settime(DivID ,times){
-	jq("#"+DivID).text(--times);
+	jQuery("#"+DivID).text(--times);
 	if (times > 0)
 		setTimeout("settime("+DivID+","+times+");", 1000);
 	else
