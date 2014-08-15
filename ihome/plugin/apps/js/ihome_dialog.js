ba2af047375cb8aa6a2367e6ecb317568fd495c7
PopPopRet <lance.through@the.heart>
2014-08-15 13:23:14 +0800

    save

--- ihome/plugin/apps/js/ihome_dialog.js
+++ ihome/plugin/apps/js/ihome_dialog.js
@@ -1,31 +1,31 @@
 /*ihome_dialog.js*/
 
-$(function() {
+jq(function() {
 	var html = "<div id=\"msg-container\"><div id=\"msg-box\"></div></div>";
-	$("body").append(html);
+	jq("body").append(html);
 	
 });
 
 
 
 function resetDialog(){
-	var msg_box = $("#msg-box");
-	var msg = $("#msg");
+	var msg_box = jq("#msg-box");
+	var msg = jq("#msg");
 	
 	//取得浏览器窗口高宽
-	var fullheight = $(window).height();
-	var fullwidth = $(window).width();
+	var fullheight = jq(window).height();
+	var fullwidth = jq(window).width();
 	
 	//取得子DIV器高宽
-	var msgWidth = $(msg).width();
-	var msgHeight = $(msg).height();
+	var msgWidth = jq(msg).width();
+	var msgHeight = jq(msg).height();
 	
 	//计算top和left
 	var msgTop = (fullheight - msgHeight - 200) / 2;
 	var msgLeft = (fullwidth - msgWidth) / 2;
 	
 	msg_box.css("top" , msgTop);
-	if(!($.browser.msie&&($.browser.version == "7.0"))){
+	if(!(jq.browser.msie&&(jq.browser.version == "7.0"))){
 		msg_box.css("left" , msgLeft);
 	} 
 	msg_box.css("width" , msgWidth + "px");
@@ -36,22 +36,22 @@ function resetDialog(){
 
 function showDialog(){
 	resetDialog();
-	$("#msg-container").fadeIn(500);
+	jq("#msg-container").fadeIn(500);
 	//可拖动
-	$("#msg-box").draggable({opacity: 0.6,cursor: 'move',handle:'#handel'});
+	jq("#msg-box").draggable({opacity: 0.6,cursor: 'move',handle:'#handel'});
 	//监听esc按键
-	$(document).bind("keydown", "esc", function (ev) { closeDialog();});
+	jq(document).bind("keydown", "esc", function (ev) { closeDialog();});
 }
 
 function closeDialog(){
-	$("#msg-container").fadeOut(500 ,function(){
-		$("#msg-box").empty();
+	jq("#msg-container").fadeOut(500 ,function(){
+		jq("#msg-box").empty();
 	});
 	
 }
 
 function settime(DivID ,times){
-	$("#"+DivID).text(--times);
+	jq("#"+DivID).text(--times);
 	if (times > 0)
 		setTimeout("settime("+DivID+","+times+");", 1000);
 	else
