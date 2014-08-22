a0381e0c98e0762c57e39f76fca47d5a94762ace
PopPopRet <lance.through@the.heart>
2014-08-22 11:28:34 +0800

    steal

--- ihome/plugin/apps/mine.php
+++ ihome/plugin/apps/mine.php
@@ -20,6 +20,31 @@ $multi = multi($count, $perpage, $page, $url);
 $query = $_SGLOBAL['db']->query("SELECT * FROM ".tname('apps_users')." JOIN ".tname('apps')." WHERE ".tname('apps_users').".uid=$uid AND ".tname('apps').".id=".tname('apps_users').".appsid LIMIT ".$start.",".$perpage);
 while($value = $_SGLOBAL['db']->fetch_array($query)) {
 	$value['logo'] = $value['logo'] ? $_SC['attachurl'].$value['logo'] : 'plugin/apps/images/app.gif';
+	switch($value['type']){
+		case 1:
+			$value['type'] = '教学类';
+			break;
+		case 2:
+			$value['type'] = '科研类';
+			break;
+		case 3:
+			$value['type'] = '财务类';
+			break;
+		case 4:
+			$value['type'] = '人力资源类';
+			break;
+		case 5:
+			$value['type'] = '资产类';
+			break;
+		case 6:
+			$value['type'] = '生活服务类';
+			break;
+		case 7:
+			$value['type'] = '其他';
+			break;
+		default:
+			break;
+	}
 	$list[] = $value;
 }
 
