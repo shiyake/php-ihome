70b1171d32f6d92182d2a0ff8f80efa7d4e96b2f
PopPopRet <lance.through@the.heart>
2014-09-04 18:48:13 +0800

    fix bug

--- ihome/plugin/apps/detail.php
+++ ihome/plugin/apps/detail.php
@@ -211,16 +211,44 @@ if($authorize && !$isAuthorized){
 }
 
 // autoAuth
-if(!@include_once(S_ROOT.'./plugin/iauth/IAuthManage.php')){//echo 'notinclude'; exit();
-    header("Location:plugin.php?pluginid=apps&ac=detail&appsid=$appsid");exit();
-}
-try {
-    $autoAuth = GetAppInfo($appsid,'auto_auth');
-} catch(IAuthException $e) {
-    //echo $e->getMessage();
-}
+$autoAuth = autoAuth($app['iauth_id']);
 
+function autoAuth($iauth_id)
+{
+    define("IAUTH_DB_USER","root");
+    define("IAUTH_DB_HOST","211.71.14.65");
+    define("IAUTH_DB_PASSWD","devihome");
+    define("IAUTH_DB_DB","iauthServer2");
 
+    define("IAUTH_VERSION",2.0);
+
+    define("IAUTH_TIME_OFFSET",120);
+    define("IAUTH_UAC_AUTH_DELAY_TIME",120);
+    define("IAUTH_WSC_AUTH_DELAY_TIME",60);
+    define("IAUTH_WSC_LOGIN_DELAY_TIME",60);
+
+    define("IAUTH_ERROR_LOG_FILE",dirname(__FILE__)."/debug/IAuthErrorLog");
+    define("IAUTH_ACCESS_LOG_FILE",dirname(__FILE__)."/debug/IAuthAccessLog");
+
+    //define("IAUTH_APP_INFO_PAGE",'http://211.71.14.65/plugin/iauth/debug/appcenter.php?ac=auth');
+    define("IAUTH_APP_INFO_PAGE",'http://i.buaa.edu.cn/plugin.php?pluginid=apps&ac=detail');
+    define("IAUTH_SIMPLE_AUTH_CONFIRM_PAGE",'http://i.buaa.edu.cn/plugin.php?pluginid=apps&ac=detail&isConfirm=1');
+    
+    if(!@include_once(S_ROOT.'./plugin/iauth/IAuthCommon.php')){
+        header("Location:plugin.php?pluginid=apps&ac=detail&appsid=$appsid");exit();
+    }
+    $autoAuth = 0;
+    if ($iauth_id) {
+        try {
+            $autoAuth = GetAppInfo($iauth_id.'','auto_auth');
+            showmessage($autoAuth);
+        } catch(IAuthException $e) {
+            $autoAuth = 0;
+            // echo $e->getMessage();
+            // exit();
+        }
+    }
+}
 
 //提交评分
 if(submitcheck('comment_submit')) {
