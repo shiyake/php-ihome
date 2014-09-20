704cedd4bca57e1361daa9e7b2d158ddf583ee8f
wangjianfei <wangjianfei@VM_48_225_centos.(none)>
2014-09-20 18:06:23 +0800

    add department rank

--- ihome/source/cp_complain.php
+++ ihome/source/cp_complain.php
@@ -5,6 +5,7 @@ if (!defined('iBUAA')) {
 }
 
 $doid = empty($_GET['doid'])?0:intval($_GET['doid']);
+$opid = empty($_GET['opid'])?0:intval($_GET['opid']);
 if ($_GET['op'] == 'delete') {
     if (submitcheck('deletesubmit')) {
         include_once(S_ROOT.'./source/function_delete.php');
@@ -13,21 +14,62 @@ if ($_GET['op'] == 'delete') {
     }
 } elseif ($_GET['op'] == 'down') {
     if (submitcheck('down')) {
-        $query = $_SGLOBAL['db']->query("select * from ".tname("complain")." where doid = $doid");
-        $complain = $_SGLOBAL['db']->fetch_array($query);
-        if (empty($complain) || $complain['uid'] != $_SGLOBAL['supe_uid'] || $complain['status'] != 1) {
+        $query = $_SGLOBAL['db']->query("select * from ".tname("complain_op")." where id = $opid");
+        $op = $_SGLOBAL['db']->fetch_array($query);
+        if (empty($op)) {
             showmessage('error_op');
         }
-        updatetable('complain', array('status'=>0), array('doid'=>$doid));
-        $oparr = array();
-        $oparr['doid'] = $doid;
-        $oparr['message'] = $message;
-        $oparr['uid'] = $_SGLOBAL['supe_uid'];
-        $oparr['username'] = $_SGLOBAL['supe_username'];
-        $oparr['optype'] = 5;
-        $oparr['dateline'] = $_SGLOBAL['timestamp'];
-        inserttable('complain_op', $oparr);
- 
+        $query = $_SGLOBAL['db']->query("select * from ".tname("complain")." where doid = $doid");
+        $complain = $_SGLOBAL['db']->fetch_array($query);
+        if (empty($complain)) {
+            showmessage('error_op');
+        }
+        $query = $_SGLOBAL['db']->query("select * from ".tname("complain_op_updown")." where opid = $op[id] and uid = $_SGLOBAL[supe_uid] and updown = 2");
+        $down = $_SGLOBAL['db']->fetch_array($query);
+        if (!empty($down)) {
+            showmessage('updown_again', $_POST['refer'], 1);
+        }
+        $downarr = array();
+        $downarr['opid'] = $opid;
+        $downarr['uid'] = $_SGLOBAL['supe_uid'];
+        $downarr['updown'] = 2;
+        $downarr['username'] = $_SGLOBAL['supe_username'];
+        $downarr['dateline'] = $_SGLOBAL['timestamp'];
+        inserttable("complain_op_updown", $downarr);
+        $_SGLOBAL['db']->query("update ".tname('complain_op')." set downnum=downnum+1 where id=$opid");
+        if ($complain['uid'] == $_SGLOBAL['supe_uid'] && $complain['lastopid'] == $opid && $complain['status'] == 1) {
+            updatetable('complain', array('status' => 0), array('id'=>$complain['id']));
+            $oparr = array();
+            $oparr['doid'] = $doid;
+            $oparr['message'] = '';
+            $oparr['uid'] = $_SGLOBAL['supe_uid'];
+            $oparr['username'] = $_SGLOBAL['supe_username'];
+            $oparr['optype'] = 5;
+            $oparr['dateline'] = $_SGLOBAL['timestamp'];
+            inserttable("complain_op", $oparr);
+        }
+        showmessage('do_success', $_POST['refer'], 0);
+    }
+} elseif ($_GET['op'] == 'up') {
+    if (submitcheck('up')) {
+        $query = $_SGLOBAL['db']->query("select * from ".tname("complain_op")." where id = $opid");
+        $op = $_SGLOBAL['db']->fetch_array($query);
+        if (empty($op)) {
+            showmessage('error_op');
+        }
+        $query = $_SGLOBAL['db']->query("select * from ".tname("complain_op_updown")." where opid = $op[id] and uid = $_SGLOBAL[supe_uid] and updown = 1");
+        $up = $_SGLOBAL['db']->fetch_array($query);
+        if (!empty($up)) {
+            showmessage('updown_again', $_POST['refer'], 1);
+        }
+        $downarr = array();
+        $downarr['opid'] = $opid;
+        $downarr['uid'] = $_SGLOBAL['supe_uid'];
+        $downarr['updown'] = 1;
+        $downarr['username'] = $_SGLOBAL['supe_username'];
+        $downarr['dateline'] = $_SGLOBAL['timestamp'];
+        inserttable("complain_op_updown", $downarr);
+        $_SGLOBAL['db']->query("update ".tname('complain_op')." set upnum=upnum+1 where id=$opid");
         showmessage('do_success', $_POST['refer'], 0);
     }
 } elseif ($_GET['op'] == 'continue') {
@@ -124,14 +166,39 @@ if ($_GET['op'] == 'delete') {
         $oparr['optype'] = $optype;
         $oparr['dateline'] = $_SGLOBAL['timestamp'];
         if ($complain['status'] == 0) {
-            if ($optype == 3) {
-                updatetable('complain', array('curuid'=>$_POST['relay_depid']), array('doid'=>$doid));
-            } elseif ($optype == 2) {
-                updatetable('complain', array('status'=>1), array('doid'=>$doid));
-            } else {
+            if ($optype != 3 && $optype != 2) {
                 showmessage('error_op');
             }
-            inserttable('complain_op', $oparr);
+            $opid = inserttable('complain_op', $oparr, true);
+            if ($optype == 3) {
+                if ($complain["curuid"] != $_SGLOBAL['supe_uid']) {
+                    showmessage('error_op');
+                }
+                updatetable('complain', array('curuid'=>$_POST['relay_depid'], 'lastopid'=>$opid), array('doid'=>$doid));
+            } elseif ($optype == 2 && $_SGLOBAL['supe_uid'] == $complain['curuid']) {
+                updatetable('complain', array('status'=>1, 'lastopid'=>$opid), array('doid'=>$doid));
+                if ($complain['lastopid'] == 0) {
+                    $result = $_SGLOBAL['db']->query("select * from ".tname('complain_dep')." where uid = $_SGLOBAL[supe_uid]");
+                    $dep = $_SGLOBAL['db']->fetch_array($result);
+                    if (empty($dep)) {
+                        $arr = array();
+                        $arr['uid'] = $_SGLOBAL['supe_uid'];
+                        $arr['username'] = $_SGLOBAL['supe_username'];
+                        $arr['upnum'] = 0;
+                        $arr['downnum'] = 0;
+                        $arr['allreplynum'] = 1;
+                        $arr['allreplysecs'] = $_SGLOBAL['timestamp'] - $complain['addtime'];
+                        $arr['score'] = 0;
+                        $arr['aversecs'] = 0;
+                        $arr['lastupdate'] = 0;
+                        inserttable('complain_dep', $arr);
+                    } else {
+                        $arr['allreplynum'] = $dep['allreplynum'] + 1;
+                        $arr['allreplysecs'] = $dep['allreplysecs'] + $_SGLOBAL['timestamp'] - $complain['addtime'];
+                        updatetable("complain_dep", $arr, array('uid'=>$_SGLOBAL['supe_uid']));
+                    }
+                }
+            }
             showmessage('do_success', $_POST['refer'], 0);
         }
     }
