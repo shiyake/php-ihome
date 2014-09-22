f0b8955b08c9306479cc027cd5c8857289c02ad2
PopPopRet <lance.through@the.heart>
2014-09-22 14:22:01 +0800

    save

--- ihome/api/im/show.php
+++ ihome/api/im/show.php
@@ -1,9 +1,15 @@
 <?php 
+	include_once('../../common.php');
+ 	include_once(S_ROOT.'./source/function_space.php');
+	include_once('verify.php');
+
 	require 'Predis/Autoloader.php';
 	Predis\Autoloader::register();
 	$client = new Predis\Client();
 	echo 'T3: '.$client->get('T3')."<br>";
 	echo 'R3: '.$client->get('R3')."<br>";
-	echo 'T247: '.$client->get('T247')."<br>";
-	echo 'R247: '.$client->get('R247')."<br>";
+	echo 'T11: '.$client->get('T11')."<br>";
+	echo 'R11: '.$client->get('R11')."<br>";
+
+	echo getfriendstatus(11,'3') <0 
 ?>
