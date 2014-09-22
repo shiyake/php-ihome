27fc18218d4f7cf2f680c469b62413a2e2b44e17
PopPopRet <lance.through@the.heart>
2014-09-22 18:03:41 +0800

    history

---
+++ ihome/api/im/history.php
@@ -0,0 +1,32 @@
+<?php 
+	include_once('../../common.php');
+ 	include_once(S_ROOT.'./source/function_space.php');
+	include_once('verify.php');
+
+	$uid = $_SGLOBAL['supe_uid'];
+	$result = array();
+	$result['status'] = 0;
+	if ($_POST['id']) {
+		$fuid = intval($_POST['id']);
+		$q = $_SGLOBAL['db']->query("select * from ".UC_DBTABLEPRE."pms where msgtoid in ($uid, $fuid) and msgfromid in ($uid, $fuid) and related and !new order by pmid limit 30");
+
+		$data = array();
+		while ($r = $_SGLOBAL['db']->fetch_array($q)) {
+			$datum = array();
+			$datum['message'] = $r['message'];
+			$datum['id'] = $r['msgfromid'];
+			$datum['time'] = $r['dateline'].'000';
+
+			$data[] = $datum;
+		}
+
+		if ($data) {
+			$result['status'] = 1;
+			$result['data'] = $data;
+		}
+	}
+
+	$history = json_encode($result);
+	echo $history;
+	exit();
+?>
