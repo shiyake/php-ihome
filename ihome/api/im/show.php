8a6612b7046a628277caea372dff797a39b3e21b
PopPopRet <lance.through@the.heart>
2014-09-23 15:16:42 +0800

    save

--- ihome/api/im/show.php
+++ ihome/api/im/show.php
@@ -8,6 +8,6 @@
 	$client = new Predis\Client();
 	echo 'T3: '.$client->get('T3')."<br>";
 	echo 'R3: '.$client->get('R3')."<br>";
-	echo 'T11: '.$client->get('T11')."<br>";
-	echo 'R11: '.$client->get('R11')."<br>";
+	echo 'T247: '.$client->get('T247')."<br>";
+	echo 'R247: '.$client->get('R247')."<br>";
 ?>
