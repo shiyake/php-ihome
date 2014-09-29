873cd7c6743861a62264eed8ae9ced7982cc73a8
PopPopRet <lance.through@the.heart>
2014-09-29 17:47:06 +0800

    save

--- ihome/source/cp_complain.php
+++ ihome/source/cp_complain.php
@@ -162,7 +162,7 @@ if ($_GET['op'] == 'delete') {
     showmessage('error_op');
 } elseif ($_GET['op'] == 'updateduty') {
     if (submitcheck("updateduty")) {
-        updatetable("complain_dep", array('depduty'=>$_POST['duty']), array('uid'=>$_POST['uid']));
+        updatetable("complain_dep", array('depduty'=>htmlentities($_POST['duty'])), array('uid'=>$_POST['uid']));
     }
 } elseif ($_GET['op'] == 'add') {
     if (submitcheck('addsubmit')) {
