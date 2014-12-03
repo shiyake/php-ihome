8c93b5c8760007911c53a2994ac8f05f2f297bac
PopPopRet <lance.through@the.heart>
2014-12-03 14:37:21 +0800

    untrack some files

--- ihome/data/log/201411_sendmail.php
+++
@@ -1,3 +0,0 @@
-<?PHP exit;?>	2014-11-13 22:44:52		127.0.0.1	15	/do.php?ac=sendmail&rand=1415889891	xuxing66@buaa.edu.cn sendmail failed.
-<?PHP exit;?>	2014-11-24 21:50:15		127.0.0.1	3	/do.php?ac=sendmail&rand=1416837009	woaizhongfeiyu@gmail.com sendmail failed.
-<?PHP exit;?>	2014-11-26 22:18:30		127.0.0.1	3	/do.php?ac=sendmail&rand=1417011494	xuxing66@buaa.edu.cn sendmail failed.
